ebb331f85aaf4a34ffa212ce4661abf79ad1be21
aditya2072 <aditya2072@proton.me>
2022-09-15 20:20:30 +0530

    added stop timer

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 const defaultTimer=25;
 let time=defaultTimer*60;
-let startBtn=document.querySelector('#start-btn');
 let stopBtn=document.querySelector("#stop-btn")
 const counter=document.querySelector('#timer');
+
 
 
 const changeTimer=()=>{
@@ -12,7 +12,14 @@ const changeTimer=()=>{
     counter.innerHTML=`${minutes}:${seconds}`;
    time--;
 }
+let startBtn=document.querySelector('#start-btn');
+let repeater;
+
 startBtn.addEventListener('click',function (){
-    setInterval(changeTimer,1000)
+   repeater= setInterval(changeTimer,1000)
+});
+
+stopBtn.addEventListener("click",()=>{
+    clearInterval(repeater)
 });
 
