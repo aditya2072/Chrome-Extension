f0ec6e83453152811fb8138521cfe5425216dfbf
aditya2072 <aditya2072@proton.me>
2022-09-17 19:31:03 +0530

    updated file structure

--- popup.js
+++ popup.js
@@ -1,19 +1,17 @@
 /** @format */
 
-let defaultTimer;
+let defaultTimer = 25
 let time = defaultTimer * 60
-let startBtn=document.querySelector('#start-btn')
+let startBtn = document.querySelector('#start-btn')
 let stopBtn = document.querySelector('#stop-btn')
 const counter = document.querySelector('#timer')
 const focus = document.querySelector('#work')
 const halt = document.querySelector('#shortbreak')
 const cease = document.querySelector('#longbreak')
-let audioFocus=new Audio('focussound.mp3')
-let audioLongBreak=new Audio('calm.mp3')
-let audioStartBtn=new  Audio('start.mp3') 
-let audioStopBtn=new Audio('stopbtn.mp3')
-
-
+let audioFocus = new Audio('./sounds/focussound.mp3')
+let audioLongBreak = new Audio('./sounds/calm.mp3')
+let audioStartBtn = new Audio('./sounds/start.mp3')
+let audioStopBtn = new Audio('./sounds/stopbtn.mp3')
 
 const changeTimer = () => {
 	const minutes = Math.floor(time / 60)
@@ -26,37 +24,34 @@ const changeTimer = () => {
 let repeater
 startBtn.addEventListener('click', function () {
 	repeater = setInterval(changeTimer, 1000)
-    audioStartBtn.play()
+	audioStartBtn.play()
 })
 
 stopBtn.addEventListener('click', () => {
 	clearInterval(repeater)
-    audioStopBtn.play()
-    audioFocus.pause()
-    audioLongBreak.pause()
+	audioStopBtn.play()
+	audioFocus.pause()
+	audioLongBreak.pause()
 })
 
-
 document.addEventListener('click', (e) => {
-	if (!e.target.matches('.btn2')) return;
-		if (e.target.matches('#work')) {
-			counter.innerHTML = '25:00'
-			defaultTimer = 25
-			time = defaultTimer * 60
-            audioFocus.play()
-            document.body.style.backgroundColor = "#92eeb7aa";
-
-		} else if (e.target.matches('#shortbreak')) {
-			counter.innerHTML = '5:00'
-			defaultTimer = 5
-			time = defaultTimer * 60
-            document.body.style.backgroundColor = "black";
-
-		} else if (e.target.matches('#longbreak')) {
-			counter.innerHTML = '15:00'
-			defaultTimer = 15
-			time = defaultTimer * 60
-            audioLongBreak.play();
-            document.body.style.backgroundColor = "#FFFF00";
-		}
-});
+	if (!e.target.matches('.btn2')) return
+	if (e.target.matches('#work')) {
+		counter.innerHTML = '25:00'
+		defaultTimer = 25
+		time = defaultTimer * 60
+		audioFocus.play()
+		document.body.style.backgroundColor = '#92eeb7aa'
+	} else if (e.target.matches('#shortbreak')) {
+		counter.innerHTML = '5:00'
+		defaultTimer = 5
+		time = defaultTimer * 60
+		document.body.style.backgroundColor = 'black'
+	} else if (e.target.matches('#longbreak')) {
+		counter.innerHTML = '15:00'
+		defaultTimer = 15
+		time = defaultTimer * 60
+		audioLongBreak.play()
+		document.body.style.backgroundColor = '#FFFF00'
+	}
+})
