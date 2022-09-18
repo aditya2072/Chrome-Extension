2202f005d644799740aeedc2493ccd140555b5c5
Swati <swatic019@gmail.com>
2022-09-18 17:08:21 +0530

    Added focus storage

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # fOCUS-dORO( Chrome-Extension)
-
+<p>In our first week of Hackathon. We have created Pomodoro Timer Chrome Extension</P>
 ### mockup video (here)
+
 
 ### about pomodoro
--- popup.js
+++ popup.js
@@ -13,48 +13,67 @@ let audioLongBreak = new Audio("./sounds/calm.mp3");
 let audioStartBtn = new Audio("./sounds/start.mp3");
 let audioStopBtn = new Audio("./sounds/stopbtn.mp3");
 
-let repeater;
-startBtn.addEventListener("click", function () {
-	repeater = setInterval(changeTimer, 1000);
-	audioStartBtn.play();
+chrome.storage.local.get(["remainingSec", "startTime"], function (result) {
+  if (result.remainingSec && result.startTime) {
+    let popupOffDuration = (new Date().getTime() - result.startTime) / 1000;
+    let popupTime = result.remainingSec - popupOffDuration;
+    if (popupTime > 0) {
+      time = popupTime;
+      changeTimer();
+	  startTimer();
+    }
+  }
 });
 
+let repeater;
+startBtn.addEventListener("click", startTimer);
+
+function startTimer() {
+  if (repeater) {
+    clearInterval(repeater);
+  }
+  repeater = setInterval(changeTimer, 1000);
+  audioStartBtn.play();
+  chrome.storage.local.set(
+    { remainingSec: time, startTime: new Date().getTime() },
+    function () {}
+  );
+}
+
 stopBtn.addEventListener("click", () => {
-	clearInterval(repeater);
-	audioStopBtn.play();
-	audioFocus.pause();
-	audioLongBreak.pause();
+  clearInterval(repeater);
+  audioStopBtn.play();
+  audioFocus.pause();
+  audioLongBreak.pause();
 });
 
 document.addEventListener("click", (e) => {
-	if (!e.target.matches(".btn2")) return;
-	if (e.target.matches("#work")) {
-		counter.innerHTML = "25:00";
-		defaultTimer = 25;
-		time = defaultTimer * 60;
-		audioFocus.play();
-		document.body.style.backgroundColor = "#92eeb7aa";
-	} else if (e.target.matches("#shortbreak")) {
-		counter.innerHTML = "5:00";
-		defaultTimer = 5;
-		time = defaultTimer * 60;
-		document.body.style.backgroundColor = "black";
-	} else if (e.target.matches("#longbreak")) {
-		counter.innerHTML = "15:00";
-		defaultTimer = 15;
-		time = defaultTimer * 60;
-		audioLongBreak.play();
-		document.body.style.backgroundColor = "#FFFF00";
-	}
+  if (!e.target.matches(".btn2")) return;
+  if (e.target.matches("#work")) {
+    counter.innerHTML = "25:00";
+    defaultTimer = 25;
+    time = defaultTimer * 60;
+    audioFocus.play();
+	clearInterval(repeater);
+    document.body.style.backgroundColor = "#92eeb7aa";
+  } else if (e.target.matches("#shortbreak")) {
+    counter.innerHTML = "5:00";
+    defaultTimer = 5;
+    time = defaultTimer * 60;
+    document.body.style.backgroundColor = "black";
+  } else if (e.target.matches("#longbreak")) {
+    counter.innerHTML = "15:00";
+    defaultTimer = 15;
+    time = defaultTimer * 60;
+    audioLongBreak.play();
+    document.body.style.backgroundColor = "#FFFF00";
+  }
 });
 
 const changeTimer = () => {
-	const minutes = Math.floor(time / 60);
-	let seconds = time % 60;
-	seconds: seconds < 10 ? "0" + seconds : seconds;
-	counter.innerHTML = `${minutes}:${seconds}`;
-	time--;
-	chrome.storage.local.set({currentTime:time}, function(){
-	  console.log(time);
-	} )
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.round(time % 60);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  counter.innerHTML = `${minutes}:${seconds}`;
+  time--;
 };
